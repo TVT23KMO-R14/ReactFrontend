8b84c1da06df0d7c1a9f3475af0b30a99f416fd3
mtintti <tiina.mantykivi@gmail.com>
2024-04-16 16:41:07 +0300

    viive haku ja footerin lisäys

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import './Footer.css'
 
 export default function Footer() {
@@ -7,4 +7,37 @@ export default function Footer() {
             <h2>Footer</h2>
     </div>
   )
+}
+*/
+
+import React from 'react';
+import './Footer.css';
+import logo from '../image.png';  // Adjust this path to the exact location of your image within the src folder
+
+export default function Footer() {
+  return (
+    <footer className="text-white py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-6">
+            <h4>About Us</h4>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur molestias quasi architecto est obcaecati hic quos totam porro, corporis delectus. 
+            </p>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <h4>Legal</h4>
+            <ul className="list-unstyled">
+              <li className="text-white">Terms of use</li>
+              <li className="text-white">Privacy policy</li>
+              <li className="text-white">DMCA</li>
+            </ul>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <img src={logo} alt="Logo" style={{ maxWidth: '300px' }} />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }

47737f5cde8d117dc80902312e79314dd32a7579
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-04-24 11:31:43 +0300

    moviepage ja seriespage odottelevat käyttöä

---
+++ src/pages/SeriesPage.js
@@ -0,0 +1,39 @@
+import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import MovieCard from '../components/MovieCard'
+import ReviewMovie from '../components/ReviewMovie'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+export default function SeriesView() { 
+    
+        const [movie, setMovie] = useState(null)
+        const { id } = useParams()
+        const navigate = useNavigate()
+    
+        React.useEffect(() => {
+            const fetchMovie = async () => {
+                try {
+                    const response = await axios.get(process.env.REACT_APP_SERVER_URL + 'search/oneseries', {
+                        params: { id }
+                    })
+                    if(response.data.length === 0) return console.log('No movie found:' + response)
+                    setMovie(response.data)
+                } catch (error) {
+                    console.error('Error fetching movie', error)
+                }
+            }
+            fetchMovie()
+        }, [id])
+    
+        if (!movie) return null
+    
+        return (
+            <div>
+                <MovieCard movie={movie} />
+                <ReviewMovie movie={movie} />
+            </div>
+        )
+    
+    }
+
